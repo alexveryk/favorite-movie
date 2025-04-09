2aea270a8e00f71a76f2e0cd740d71adacae1edf
Oleksandr Veryk <veryk.av@gmail.com>
2025-04-09 23:58:09 +0300

    add routers

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,13 @@
+import { Menu } from "../Menu/Menu";
 import styles from "./Layout.module.css";
 
 export const Layout = ({ children }) => {
   console.log(children);
   return (
     <div className={styles.layout}>
-      <header className={styles.layoutHeader}>Header</header>
+      <header className={styles.layoutHeader}>
+        <Menu />
+      </header>
       <div className={styles.layoutContent}>
         <aside className={styles.layoutSidebar}>Sidebar</aside>
         <main className={styles.layoutMain}>{children}</main>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./Menu.module.css";
+
 export const Menu = () => {
   return (
     <nav>
-      <ul>
-        <li>Фільми</li>
-        <li>Серіали</li>
-      </ul>
+      <Link to="/" className={styles.link}>
+        Home
+      </Link>
+      <Link to="/movies" className={styles.link}>
+        Movies
+      </Link>
+      <Link to="/serials" className={styles.link}>
+        Serials
+      </Link>
     </nav>
   );
 };
